38ac31740d8f4b98c90a7ffd75fa4851083dd8df
7134g <987712030@qq.com>
2019-08-23 00:12:51 +0800

    speed

--- mh/dldl.py
+++ mh/dldl.py
@@ -1,45 +1,65 @@
 import requests
-import re
 import os
-import time
+import shutil
+from concurrent.futures import ProcessPoolExecutor,ThreadPoolExecutor
+from threading import currentThread
+import traceback
+from PIL import Image
 
-def baocun_img(zidian,book_name):
-        n = 0
-        for i, k in zidian.items():
-            path = r"C:\Users\Administrator\Desktop\斗罗大陆\{}".format(book_name[n])
-            os.makedirs(path)
 
-            for pn_pn in range(1, int(k)+1):
-                url = 'https://mhpic.jumanhua.com/comic/D%2F%E6%96%97%E7%BD%97%E5%A4%A7%E9%99%86%E6%8B%86%E5%88%86%E7%89%88%2F{}%E8%AF%9DGQ%2F{}.jpg-mht.middle'.format(i,pn_pn)
-                re = requests.get(url)
-                with open(path+'\{}{}.jpg'.format(book_name[n], pn_pn),'wb') as f:
-                    f.write(re.content)
-                    f.close()
-            print(book_name[n],'已经完成')
-            n += 1
-            time.sleep(1)
 
-def book_url():
-    url = 'https://www.manhuatai.com/douluodalu/'
+def save_image(input_name):
+    im = Image.open(input_name)
+    if im.mode=="RGBA":
+        im.load()  # required for png.split()
+        background = Image.new("RGB", im.size, (255, 255, 255))
+        background.paste(im, mask=im.split()[3])  # 3 is the alpha channel
+        im = background
+    im.save(input_name.replace('.webp', '.jpg'),'JPEG')
+    os.remove(input_name)
 
-    res = requests.get(url)
-    res.encoding = 'utf-8'
-    book_nam = re.findall('title="第\d+话\s\S+"', str(res.text))#得到每一话的名字
-    book_ = re.findall('第\d+话\s\S+\(\d+P\)', str(res.text))
-    book_p =re.findall('\d+P', str(book_))
-    book_pn = re.findall('\d+', str(book_p))
 
-    book_name1 = re.sub(r'title="|\s|\（|\）|"', '', str(book_nam))
-    book_name = re.findall(r"'(.+?)'",book_name1)#将字符串转化为list
+def get_task():
+    url = 'https://www.manhuatai.com/api/getComicInfoBody?product_id=2&productname=mht&platformname=pc&comic_id=25933'
 
-    book_i = re.findall(r"\d+话", book_name1)
-    book_id =re.findall(r'\d+', str(book_i))
+    response = requests.get(url)
+    response_json = response.json()
+    tasks = response_json['data']['comic_chapter']
 
-    zidian = dict(zip(book_id,book_pn))
+    # pool = ThreadPoolExecutor(max_workers=2)
+    # for task in tasks:
+    #     pool.submit(deal_task,task)
+    # pool.shutdown()
 
-    baocun_img(zidian,book_name)
+    with ProcessPoolExecutor(max_workers=2) as pool:
+        for index,task in enumerate(tasks[::-1]):
+            pool.submit(deal_task, index, task)
+
+
+
+def deal_task(index, task):
+    # for index,task in enumerate(tasks[::-1]):
+    print(currentThread())
+    page_count = task['end_num']
+    page_name = task['chapter_name']
+    path = "../download/dldl/{}".format(page_name)
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+    for count in range(page_count):
+        img_url = "https://mhpic.manhualang.com/comic/D/%E6%96%97%E7%BD%97%E5%A4%A7%E9%99%86%E6%8B%86%E5%88%86%E7%89%88/{}话/{}.jpg-mht.middle.webp".format(index+1,count+1)
+        response_img = requests.get(img_url)
+
+        img_path = path + '/{}_{}.webp'.format(page_name,count)
+        with open(img_path, 'wb') as f:
+            f.write(response_img.content)
+            f.close()
+        save_image(img_path)
+
+    print(page_name, '已经完成')
+
 
 
 if __name__ == '__main__':     
-        book_url()
+    get_task()
 

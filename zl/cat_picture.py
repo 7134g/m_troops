879d4d46a3fe12c96e89c441a5a05d06827c83bc
7134g <987712030@qq.com>
2019-08-23 21:39:54 +0800

    delete_useless

--- zl/cat_picture.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-response = urllib.request.urlopen('http://placekitten.com/g/500/600')
-cat_img = response.read()
-
-with open('cat_500_600.jpg','wb') as f:
-    f.write(cat_img)

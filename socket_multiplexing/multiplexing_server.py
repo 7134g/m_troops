d49564abf5127dbb416aed73e89f55020265c1f6
7134g <987712030@qq.com>
2019-08-28 21:20:21 +0800

    updata

--- socket_multiplexing/multiplexing_server.py
+++ socket_multiplexing/multiplexing_server.py
@@ -1,58 +1,97 @@
 import socket
-from selectors import DefaultSelector
+from selectors import DefaultSelector, EVENT_READ, EVENT_WRITE
 from urllib.parse import urlparse
 
-selector = DefaultSelector()
+from _decorator import timer
 
 
-
-
-
-class Request:
-    def __init__(self, headers: dict):
+class Feture:
+    def __init__(self, headers = None):
+        self.spider_url = ''
+        self.host = ''
+        self.path = ''
+        self.data = b''
         self.headers = headers
+        # self.client = ''
 
-    def get(self, url):
-        url = urlparse(url)
-        host = url.netloc
-        path = url.path
-        if not path:
-            path = '/'
+    def readable(self, key):
+        temp = self.client.recv(1024)
+        if temp:
+            self.data += temp
+        else:
+            selector.unregister(key.fd)
+            data = self.data.decode('utf8')
+            html_data = data.split('\r\n\r\n')
+            print(html_data)
+            self.client.close()
+            urls.remove(self.spider_url)
+            if not urls:
+                global stop
+                stop = True
 
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((host, 80))
-        print('开始发送')
+    def connector(self, key):
+        selector.unregister(key.fd)
 
         request_contain = ''
-        for k,v in self.headers.items():
+        for k, v in self.headers.items():
             temp = "{k}:{v}\r\n".format(k=k, v=v)
             request_contain = "".join([request_contain, temp])
-            # headers = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36\r\n'
-            # connection = 'Connection:close\r\n'
         send_data = 'GET {path} HTTP/1.1\r\nHost:{host}\r\n{request_contain}\r\n'.format(
-            path=path, host=host, request_contain=request_contain).encode('utf8')
-        client.send(send_data)
+                    path=self.path, host=self.host, request_contain=request_contain).encode('utf8')
+        print(send_data)
+        self.client.send(send_data)
 
-        data = b''
-        while True:
-            temp = client.recv(1024)
-            if temp:
-                data += temp
-            else:
-                break
-
-        data = data.decode('utf8')
-        html_data = data.split('\r\n\r\n')
-        print(html_data)
-        client.close()
+        selector.register(self.client.fileno(), EVENT_READ, self.readable)
 
 
+    def get(self, url):
+        self.spider_url = url
+        url = urlparse(url)
+        self.host = url.netloc
+        self.path = url.path
+        if not self.path:
+            path = '/'
+
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.client.connect((self.host, 80))
+        except BlockingIOError:
+            pass
+        # print('连接建立')
+
+        selector.register(self.client.fileno(), EVENT_WRITE, self.connector)
 
 
-if __name__ == '__main__':
+def loop():
+    while not stop:
+        try:
+            ready = selector.select()
+            for key, mask in ready:
+                call_back = key.data
+                call_back(key)
+        except OSError:
+            break
+
+@timer
+def main():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
         "Connection": "close",
     }
-    session = Request(headers=headers)
-    res = session.get("https://www.baidu.com/")
+    # session = Request(headers=headers)
+    # res = session.get("https://www.baidu.com/")
+
+    for index in range(10):
+        url = "http://shop.projectsedu.com/goods/{}/".format(index)
+        urls.append(url)
+        feture = Feture(headers=headers)
+        feture.get(url)
+    loop()
+
+
+selector = DefaultSelector()
+stop = False
+urls = ["https://www.baidu.com/"]
+
+if __name__ == '__main__':
+    main()

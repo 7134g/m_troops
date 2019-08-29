180ab88f1ea9b29161747441fd5fa9b98e21635a
7134g <987712030@qq.com>
2019-08-29 20:36:55 +0800

    socket

---
+++ socket_multiplexing/socket_server.py
@@ -0,0 +1,5 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
--- socket_multiplexing/socket_spider.py
+++ socket_multiplexing/socket_spider.py
@@ -1,18 +1,43 @@
 import socket
-from selectors import DefaultSelector, EVENT_READ, EVENT_WRITE
+from selectors import DefaultSelector, EVENT_READ, EVENT_WRITE # 自动采用select或者epoll
 from urllib.parse import urlparse
+import time
 
-from _decorator import timer
+
+def timer(func):
+    def wrapper(*args,**kwargs):
+        t1 = time.time()
+        v = func(*args,**kwargs)
+        t2 = time.time()
+        z_time = t2-t1
+        print("本次操作耗费{}秒".format(z_time))
+        return v
+    return wrapper
 
 
-class Feture:
-    def __init__(self, headers = None):
-        self.spider_url = ''
+class Fecher:
+    def __init__(self,headers = None):
         self.host = ''
         self.path = ''
         self.data = b''
-        self.headers = headers
         # self.client = ''
+        self.headers = headers
+
+    def connected(self, key):
+        selector.unregister(key.fd)
+
+        request_contain = ''
+        if self.headers:
+            for k, v in self.headers.items():
+                temp = "{k}:{v}\r\n".format(k=k, v=v)
+                request_contain = "".join([request_contain, temp])
+
+        send_data = 'GET {path} HTTP/1.1\r\nHost:{host}\r\n{request_contain}\r\n'.format(path=self.path, host=self.host, request_contain=request_contain).encode('utf8')
+        print("发起信息", send_data)
+
+        self.client.send(send_data)
+
+        selector.register(self.client.fileno(), EVENT_READ, self.readable)
 
     def readable(self, key):
         temp = self.client.recv(1024)
@@ -29,48 +54,33 @@ class Feture:
                 global stop
                 stop = True
 
-    def connector(self, key):
-        selector.unregister(key.fd)
-
-        request_contain = ''
-        for k, v in self.headers.items():
-            temp = "{k}:{v}\r\n".format(k=k, v=v)
-            request_contain = "".join([request_contain, temp])
-        send_data = 'GET {path} HTTP/1.1\r\nHost:{host}\r\n{request_contain}\r\n'.format(
-                    path=self.path, host=self.host, request_contain=request_contain).encode('utf8')
-        print(send_data)
-        self.client.send(send_data)
-
-        selector.register(self.client.fileno(), EVENT_READ, self.readable)
-
-
     def get(self, url):
         self.spider_url = url
         url = urlparse(url)
         self.host = url.netloc
         self.path = url.path
         if not self.path:
-            path = '/'
+            self.path = '/'
 
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.setblocking(False)  # 非阻塞
+
         try:
             self.client.connect((self.host, 80))
         except BlockingIOError:
             pass
-        # print('连接建立')
 
-        selector.register(self.client.fileno(), EVENT_WRITE, self.connector)
+        # 注册文件描述符
+        selector.register(self.client.fileno(), EVENT_WRITE, self.connected)
 
 
-def loop():
+def creat_loop():
     while not stop:
-        try:
-            ready = selector.select()
-            for key, mask in ready:
-                call_back = key.data
-                call_back(key)
-        except OSError:
-            break
+        ready = selector.select()
+        for key, mask in ready:
+            call_back = key.data
+            call_back(key)
+
 
 @timer
 def main():
@@ -78,20 +88,18 @@ def main():
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
         "Connection": "close",
     }
-    # session = Request(headers=headers)
-    # res = session.get("https://www.baidu.com/")
 
     for index in range(10):
-        url = "http://shop.projectsedu.com/goods/{}/".format(index)
+        url = 'http://shop.projectsedu.com/goods/{}/'.format(index)
         urls.append(url)
-        feture = Feture(headers=headers)
+        feture = Fecher(headers=headers)
         feture.get(url)
-    loop()
+    creat_loop()
 
 
 selector = DefaultSelector()
+urls = []
 stop = False
-urls = ["https://www.baidu.com/"]
 
 if __name__ == '__main__':
     main()

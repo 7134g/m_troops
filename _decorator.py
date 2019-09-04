0d1d83b82f877991bd4f9872e61d85bce9e3e99c
7134g <987712030@qq.com>
2019-09-04 21:17:32 +0800

    添加类日志编写器

--- _decorator.py
+++ _decorator.py
@@ -3,16 +3,18 @@ import os
 import logging
 import traceback
 
+
 # 计时器
 def timer(func):
-    def wrapper(*args,**kwargs):
+    def wrapper(*args, **kwargs):
         t1 = time.time()
-        v = func(*args,**kwargs)
+        v = func(*args, **kwargs)
         t2 = time.time()
         z_time = t2-t1
         print("本次操作耗费{}秒".format(z_time))
         return v
     return wrapper
+
 
 # 日志编写器
 def write_log(name):
@@ -20,7 +22,8 @@ def write_log(name):
         def get_log(*args, **kwargs):
             logger = logging.getLogger(__name__)
             logger.setLevel(level=logging.INFO)
-            handler = logging.FileHandler(os.path.split(os.path.realpath(__file__))[0] + '\\log\\' + name + ".txt",encoding="utf-8")
+            path = os.path.split(os.path.realpath(__file__))[0]
+            handler = logging.FileHandler(path + '\\log\\' + name + ".txt", encoding="utf-8")
             handler.setLevel(logging.INFO)
             formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
             handler.setFormatter(formatter)
@@ -34,3 +37,33 @@ def write_log(name):
     return wrapper
 
 
+# 日志编写器2
+class LoggerManage:
+    def __init__(self, name):
+        self.name = name
+
+    def __call__(self, func):
+        def wrapper(*args, **kwargs):
+            logger = logging.getLogger(__name__)
+            logger.setLevel(level=logging.INFO)
+            path = os.path.split(os.path.realpath(__file__))[0]
+            handler = logging.FileHandler(path + '\\log\\' + self.name + ".txt", encoding="utf-8")
+            handler.setLevel(logging.INFO)
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+            try:
+                result = func(*args, **kwargs)
+            except:
+                logger.error(traceback.format_exc())
+            logger.info(result)
+        return wrapper
+
+
+if __name__ == '__main__':
+    @LoggerManage("error")
+    def test():
+        print("do something ...")
+        return "gogogo_finish"
+
+    test()

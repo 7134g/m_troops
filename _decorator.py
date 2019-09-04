f1e76f33f9e5ff4cad846adbc59973885304614b
7134g <987712030@qq.com>
2019-09-04 20:52:07 +0800

    添加日志编写器

--- _decorator.py
+++ _decorator.py
@@ -1,4 +1,7 @@
 import time
+import os
+import logging
+import traceback
 
 # 计时器
 def timer(func):
@@ -10,3 +13,24 @@ def timer(func):
         print("本次操作耗费{}秒".format(z_time))
         return v
     return wrapper
+
+# 日志编写器
+def write_log(name):
+    def wrapper(func):
+        def get_log(*args, **kwargs):
+            logger = logging.getLogger(__name__)
+            logger.setLevel(level=logging.INFO)
+            handler = logging.FileHandler(os.path.split(os.path.realpath(__file__))[0] + '\\log\\' + name + ".txt",encoding="utf-8")
+            handler.setLevel(logging.INFO)
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+            try:
+                result = func(*args, **kwargs)
+            except:
+                logger.error(traceback.format_exc())
+            logger.info(result)
+        return get_log
+    return wrapper
+
+

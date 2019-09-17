636a9b52d0a93242058df53bee3a8fdba81f4275
7134g <987712030@qq.com>
2019-09-17 12:31:07 +0800

    go_file

---
+++ go/miscellany/cat_pictrue.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+func HandError(err error, when string)  {
+	if err != nil{
+		fmt.Println(when, err)
+		os.Exit(1)
+	}
+}
+
+func download(ch chan int)  {
+	resp, err := http.Get("http://placekitten.com/g/200/200")
+	defer resp.Body.Close()
+
+	HandError(err, "http.Get('http://placekitten.com/g/200/200')")
+	imageByte, _ := ioutil.ReadAll(resp.Body)
+
+
+	filname := "D:\\_document\\Go\\_programe\\test1\\main\\img\\" + strconv.Itoa(int(time.Now().UnixNano())) + ".jpg"
+	err = ioutil.WriteFile(filname, imageByte, 0644)
+	ch <- 1
+	if err == nil{
+		fmt.Println(filname,"下载成功！")
+	}else{
+		fmt.Println(filname,"下载失败！")
+	}
+}
+
+func main()  {
+	chs := make([] chan int, 5)
+	for n:=0;n<5;n++{
+		chs[n] = make(chan int)
+		go download(chs[n])
+	}
+
+	for _,ch := range chs{
+		<-ch
+	}
+}

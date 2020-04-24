1907fa90ab42843c5599da41fc69c22dd95cb7bb
lxc <987712030@qq.com>
2020-04-24 15:32:45 +0800

    添加装饰器写 log

--- py/log_modele/Logmanager.py
+++ py/log_modele/Logmanager.py
@@ -36,28 +36,34 @@ def get_log(name="log"):
     return logger
 
 
-def write_log(name="log"):
-    def wrapper(func):
-        def get_log(*args, **kwargs):
-            path = os.path.join(Setting.PROJECTFILE, "log", "{}.txt".format(name))
+def decorators_log(logs_path, file_name):
+    def warpper(func):
+        def main_func(*args, **kwargs):
+            path = os.path.join(logs_path, "{}.log".format(file_name))
+            if not os.path.exists(logs_path):
+                os.makedirs(logs_path)
 
             logger = logging.getLogger(__name__)
             logger.setLevel(level=logging.INFO)
+            formatter = logging.Formatter('%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s - %(message)s')
+
+            # 日志
             handler = logging.FileHandler(path, encoding="utf-8")
             handler.setLevel(logging.INFO)
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
             handler.setFormatter(formatter)
+
+            # 窗口
+            windown_handler = logging.StreamHandler()
+            windown_handler.setFormatter(formatter)
+
+            logger.addHandler(windown_handler)
             logger.addHandler(handler)
-            try:
-                result = func(*args, **kwargs)
-                logger.info(result)
-            except Exception as e:
-                logger.error(e)
-        return get_log
-    return wrapper
+            return func(*args, **kwargs)
+        return main_func
+    return warpper
 
 
-@write_log("1")
+@decorators_log("./", 'log')
 def test():
     print("ok")
     raise Exception("2222")

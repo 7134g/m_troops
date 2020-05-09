6e1c3c7b30756dbce18cbbe2358abbeb33d81996
lxc <987712030@qq.com>
2020-05-09 17:19:39 +0800

    实现 set 结构

---
+++ go/general/NewStruct.go
@@ -0,0 +1,85 @@
+type Hset struct {
+    m map[interface{}]bool
+}
+
+// 给set结构初始化
+func NewHset() *Hset {
+    return &Hset{m: make(map[interface{}]bool)}
+}
+
+func (h *Hset) Add(v interface{}) (b bool) {
+    if h.m[v]{
+        return false
+    }
+    h.m[v] = true
+    return true
+}
+
+func (h *Hset) Delete(v interface{}) {
+    delete(h.m, v)
+}
+
+func (h *Hset) Clear() {
+    h.m = make(map[interface{}]bool)
+}
+
+func (h *Hset) Contains(v interface{}) bool {
+    return h.m[v]
+}
+
+func (h *Hset) Len() int {
+    return len(h.m)
+}
+
+func (h *Hset) Same(other *Hset) bool {
+    if other == nil{
+        return false
+    }
+
+    if other.Len() != h.Len(){
+        return false
+    }
+
+    for k, _ := range h.m{
+        if h.m[k] != other.m[k]{
+            return false
+        }
+    }
+    return true
+}
+
+func (h *Hset) Elements() []interface{} {
+    length := len(h.m)
+
+    result := make([]interface{}, length)
+
+    index := 0
+
+    for k, _ := range h.m{
+        result[index] = k
+        index ++
+    }
+
+    return result
+}
+
+func (h *Hset) String() string {
+    var buf bytes.Buffer
+
+    buf.WriteString("set{")
+
+    length := h.Len()
+
+
+    for i, v := range h.Elements() {
+        buf.WriteString(fmt.Sprintf("%v", v))
+        if i < length - 1{
+            buf.WriteString(" ")
+        }
+
+    }
+
+    buf.WriteString("}")
+
+    return buf.String()
+}

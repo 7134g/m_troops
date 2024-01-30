f646213eb30b6ceeaa839f42070e2572be2f0906
7134g <987712030@qq.com>
2024-01-30 17:13:31 +0800

    fix: martian

--- go/common/proxy/martian_test.go
+++ go/common/proxy/martian_test.go
@@ -4,13 +4,6 @@ import (
 	"testing"
 )
 
-func TestServe(t *testing.T) {
-	err := Serve()
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
 func TestMartian(t *testing.T) {
 	SetServeProxyAddress("http://127.0.0.1:7890", "", "")
 	OpenCert()

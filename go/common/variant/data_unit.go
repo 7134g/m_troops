e7e820c147365a0364488a90e0eda6e38812da05
7134g <987712030@qq.com>
2024-05-14 17:38:57 +0800

    add: ByteCountIEC 将数值转化为kb，mb，gb

---
+++ go/common/variant/data_unit.go
@@ -0,0 +1,18 @@
+package variant
+
+import "fmt"
+
+// ByteCountIEC 将数值转化为kb，mb，gb
+func ByteCountIEC(b uint64) string {
+	const unit = 1024
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %ciB",
+		float64(b)/float64(div), "KMGTPE"[exp])
+}
